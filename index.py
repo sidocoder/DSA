2adc230efa39fe69cd3562f8412a24f08ce4578e
https://github.com/sidocoder/first-intern <sifhoran2024@gmail.com>
2024-11-06 22:22:57 +0300

    2nd commit

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ for i in range(8):
     print(newnum)
     start = end
     end = newnum
-#---------------------------------fabonnicci using recursion that excutes the fabonicci number when enter its index------------------------------------------
+#---------------------------------fabonnicci using recursion that excutes the fabonicci number when enter its index----------------------------------
 def F(n):
     if n <= 1:
         return n
@@ -40,8 +40,8 @@ for i in range(len(arr)):
     if arr[i]==301:
         return i
 for i in range arr:
-    print(i)""" """
-# ----------------------------------------inserting a number in array-------------------------------------------------------------------------------
+    print(i)
+# -------------------------------------inserting a number in array----------------------------------------
 import array as arr
 a = arr.array('i', [1,2,3])
 print("the array before insertion contains:", end=" ")
@@ -52,7 +52,7 @@ print() #---------this is used to add new line
 print("the array after insertion contains:", end=" ")
 for i in range (len(a)):
     print(a[i], end=" ")
-#--------------------------------removing an element from array--------------------------------------------
+#--------------------------------removing an element from array-------------------------------------------
 import array
 arr = array.array('i', [1, 2, 3, 1, 5])
 print("The new created array is : ", end="")
@@ -72,7 +72,7 @@ print("The array after removing is : ", end="")
 for i in range(0, 3):
     print(arr[i], end=" ")
 
-#----------------------------------SLICING ARRAY--------------------------------------------
+#-------------------------------------------SLICING ARRAY--------------------------------------------
 import array
 arr = array.array('i', [1,2,3,4,5,6])
 subset = arr[1:4]
@@ -90,6 +90,7 @@ print(8)
 print(13, end=" ")
 print(21)
 
+#-----------------------------------
 import array
 arr = array.array('i',[1,2,3,4,5])
 for i in range(5):
@@ -106,6 +107,7 @@ else:
 n = int(input(""))
 t = n * 2
 print(t)
+#------------------------------------------COMPARING NUMBERS-----------------------------------------------
 x = 62
 y = 62
 if x > y:
@@ -113,7 +115,7 @@ if x > y:
 elif y > x:
     print("y is greater")
 else:
-    print("both are equal")""""""
+    print("both are equal")
 n = input("")
 y = 0
 if n <= y:
@@ -123,6 +125,7 @@ elif n == y:
 else:
     print("the number is positive")
 arr = [2,3,2,3,4,5]
+#----------------------------------REMOVING ELEMENT OF SAME VALUE WITH GIVEN NUMBER---------------------
 class Solution(object):
     def removeElement(self, arr, val):
         for i in range(len(arr)):
@@ -139,19 +142,37 @@ bio = int(input("enter your bio score:"))
 average =(maths + phys +bio)/3
 
 print("average is:",average)
-#----------------------AVERAGE OF ELEMENTS-----------------------
+#-------------------------------------------------AVERAGE OF ELEMENTS------------------------------------
 a=[]
 element_number = int(input("enter the number of subjects u learnt:"))
 for i in range(element_number):
     user_input = int(input("enter your score in subjects:"))
     user_input = a.append(user_input)
     average = sum(a)/len(a)
-print(average)"""
-# ----------------------FACTORIAL--------------------------------
+print(average)
+# ----------------------------------------------------FACTORIAL---------------------------------------------
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+       
 num = int(input("enter a number:"))
-def factorial(num):
-    for i in range (num):
-        while j < num:
-            new_num = num * (num - j)
-            j =+ 1
-print(factorial())                   
+result = factorial(num)
+print(result)        
+"""
+class Solution(object):
+    def divide(dividend, divisor):
+        if dividend > divisor:
+            return dividend/ divisor
+    num1 = int(input("enter dividend: "))
+    num2 = int(input("enter divisor: "))
+    result = divide(num1, num2)
+    print(int(result))
+word = "banana"
+j = 0
+for letter in word:
+    if letter == 'a':
+        j = j + 1
+print(j)
