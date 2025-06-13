51967c9525445e880ce62aad3efa2fa63927f471
https://github.com/sidocoder/first-intern <sifhoran2024@gmail.com>
2025-06-13 23:53:23 +0300

    passed

--- A_Can_You_Reduce_It_to_One.py
+++ A_Can_You_Reduce_It_to_One.py
@@ -1,21 +1,16 @@
 t = int(input())
+
 for _ in range(t):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
 
-    if 1 not in a:
-        print("NO")
-        continue
-
-    reachable = [False] * n
-    for i in range(n):
-        if a[i] == 1:
-            for j in range(i - k + 1, i + 1):
-                if 0 <= j <= n - k:
-                    for l in range(k):
-                        reachable[j + l] = True
-
-    if all(reachable[i] or a[i] == 1 for i in range(n)):
+    found_one = False
+    for x in a:
+        if x == 1:
+            found_one = True
+            break
+    
+    if found_one:
         print("YES")
     else:
         print("NO")
